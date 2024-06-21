176b1562ef2f742bf39c36286602aa414550292d
lumierevy <lumieremahoutei01@gmail.com>
2024-06-21 17:59:12 +0700

    first commit

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <header className="fixed inset-8 flex justify-between">
+        <div>Yusuf.Co</div>
+      </header>
+      <h2 className="text-2xl font-bold">Dashboard</h2>
+    </main>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="h-screen flex justify-center items-center">
+      <header className="fixed inset-8 flex justify-between">
+        <div>Yusuf.Co</div>
+        <div>Login</div>
+      </header>
+      <h2 className="text-2xl font-bold">Landing Page Clerk + NextJS</h2>
+    </main>
+  );
+}
